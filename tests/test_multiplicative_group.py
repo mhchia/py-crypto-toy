49095fe093d510a5f0a5f65ce5c02703a0b6f390
mhchia <kevin.mh.chia@gmail.com>
2020-04-06 16:12:08 +0800

    Add zk-proofs gen/verify

--- tests/test_multiplicative_group.py
+++ tests/test_multiplicative_group.py
@@ -1,6 +1,7 @@
 import binascii
+import hashlib
 import secrets
-from typing import Sequence
+from typing import Sequence, Tuple
 
 import pytest
 
@@ -44,6 +45,7 @@ def test_generator():
     assert g.exponentiate(0) == g.identity
 
 
+endian = "big"
 modulus_repr = """
 FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
 29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
@@ -56,12 +58,137 @@ C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
 """
 modulus_hex_str = modulus_repr.replace(" ", "").replace("\n", "")
 modulus_bytes = binascii.unhexlify(modulus_hex_str)
-modulus_int = int.from_bytes(modulus_bytes, "big")
+modulus_int = int.from_bytes(modulus_bytes, endian)
+q = (modulus_int - 1) // 2
 g = MultiplicativeGroupElement(modulus_int, 2)
+modulus_size = 1536 // 8  # bytes
 
 
 def get_random_secret() -> int:
     return secrets.randbelow(modulus_int)
+
+
+def hash_to_int(*args: Sequence[int]) -> int:
+    # FIXME: Check how to serialize in the spec
+    # Consider 192 = 1536 // 8
+
+    serialized_inputs = tuple(i.to_bytes(modulus_size, endian) for i in args)
+    concated_input = b"".join(serialized_inputs)
+    h = hashlib.sha256(concated_input)
+    return int.from_bytes(h.digest(), endian)
+
+
+def gen_proof_knowledge_discrete_log(
+    version: int, g: MultiplicativeGroupElement, x: int, random_value: int
+) -> Tuple[int, int]:
+    """
+    Generate the proof of knowledge of the discrete log of an element.
+    I.e. given `y` we know such `x` satisfying `y = g^x`.
+    """
+    c = hash_to_int(version, g.exponentiate(random_value).value)
+    d = (random_value - x * c) % q
+    return c, d
+
+
+def verify_proof_knowledge_discrete_log(
+    version: int,
+    c: int,
+    d: int,
+    g: MultiplicativeGroupElement,
+    y: MultiplicativeGroupElement,
+) -> bool:
+    """
+    Verify the proof. Given `y`, `g`, `c`, and `d`, we verify whether the proof is correct.
+    """
+    return c == hash_to_int(version, g.exponentiate(d).operate(y.exponentiate(c)).value)
+
+
+def gen_proof_equality_discrete_coordinates(
+    *,
+    version: int,
+    g_0: MultiplicativeGroupElement,
+    g_1: MultiplicativeGroupElement,
+    g_2: MultiplicativeGroupElement,
+    exponent_0: int,
+    exponent_1: int,
+    random_value_0: int,
+    random_value_1: int,
+) -> Tuple[int, int, int]:
+    """
+    Given `g_0`, `g_1`, `g_2`, generate a proof that we know `exponent_0` and `exponent_1` such that
+    `y_0 = {g_0}^{exponent_0}` and `y_1 = ({g_1}^{exponent_0})*({g_2}^{exponent_1})`.
+    """
+    c = hash_to_int(
+        version,
+        g_0.exponentiate(random_value_0).value,
+        g_1.exponentiate(random_value_0)
+        .operate(g_2.exponentiate(random_value_1))
+        .value,
+    )
+    d_0 = (random_value_0 - exponent_0 * c) % q
+    d_1 = (random_value_1 - exponent_1 * c) % q
+    return c, d_0, d_1
+
+
+def verify_proof_equality_discrete_coordinates(
+    *,
+    version: int,
+    g_0: MultiplicativeGroupElement,
+    g_1: MultiplicativeGroupElement,
+    g_2: MultiplicativeGroupElement,
+    y_0: MultiplicativeGroupElement,
+    y_1: MultiplicativeGroupElement,
+    c: int,
+    d_0: int,
+    d_1: int,
+) -> bool:
+    return c == hash_to_int(
+        version,
+        g_0.exponentiate(d_0).operate(y_0.exponentiate(c)).value,
+        g_1.exponentiate(d_0)
+        .operate(g_2.exponentiate(d_1))
+        .operate(y_1.exponentiate(c))
+        .value,
+    )
+
+
+def gen_proof_equality_discrete_log(
+    version: int,
+    g_0: MultiplicativeGroupElement,
+    g_1: MultiplicativeGroupElement,
+    exponent: int,
+    random_value: int,
+) -> Tuple[int, int]:
+    c = hash_to_int(
+        version,
+        g_0.exponentiate(random_value).value,
+        g_1.exponentiate(random_value).value,
+    )
+    d = (random_value - exponent * c) % q
+    return c, d
+
+
+def verify_proof_equality_discrete_log(
+    version: int,
+    g_0: MultiplicativeGroupElement,
+    g_1: MultiplicativeGroupElement,
+    y_0: MultiplicativeGroupElement,
+    y_1: MultiplicativeGroupElement,
+    c: int,
+    d: int,
+) -> bool:
+    return c == hash_to_int(
+        version,
+        g_0.exponentiate(d).operate(y_0.exponentiate(c)).value,
+        g_1.exponentiate(d).operate(y_1.exponentiate(c)).value,
+    )
+
+
+def test_hash_to_int():
+    assert (
+        hash_to_int(1)
+        == 3938873202533110169729565480406493056982423978045281570761196615900255145854
+    )
 
 
 def test_diffie_hellman():
@@ -73,11 +200,7 @@ def test_diffie_hellman():
 
 
 @pytest.mark.parametrize(
-    "x, y",
-    (
-        (5566, 7788),
-        (5566, 5566),
-    ),
+    "x, y", ((5566, 7788), (5566, 5566),),
 )
 def test_smp(x, y):
     # TODO: Add zk proofs
@@ -89,27 +212,95 @@ def test_smp(x, y):
     a_2, a_3 = get_random_secret(), get_random_secret()
     g_2a = g_1.exponentiate(a_2)
     g_3a = g_1.exponentiate(a_3)
+    # zk proofs
+    r_2a, r_3a = get_random_secret(), get_random_secret()
+
+    c_2a, d_2a = gen_proof_knowledge_discrete_log(1, g_1, a_2, r_2a)
+    c_3a, d_3a = gen_proof_knowledge_discrete_log(2, g_1, a_3, r_3a)
 
     # Bob
+    # TODO: "Check that both g2a and g3a are >= 2 and <= modulus-2", what is the "modulus" here?
+    assert g_2a.value >= 2
+    assert g_3a.value >= 2
+
+    # Verify zk proofs
+    assert verify_proof_knowledge_discrete_log(1, c_2a, d_2a, g_1, g_2a)
+    assert verify_proof_knowledge_discrete_log(2, c_3a, d_3a, g_1, g_3a)
+
     b_2, b_3 = get_random_secret(), get_random_secret()
     g_2b = g_1.exponentiate(b_2)
     g_3b = g_1.exponentiate(b_3)
-
     g_2 = g_2a.exponentiate(b_2)
     g_3 = g_3a.exponentiate(b_3)
-    r = get_random_secret()
-    p_b = g_3.exponentiate(r)
-    q_b = g_1.exponentiate(r).operate(g_2.exponentiate(y))
+
+    r_2b, r_3b, r_4b, r_5b, r_6b = (
+        get_random_secret(),
+        get_random_secret(),
+        get_random_secret(),
+        get_random_secret(),
+        get_random_secret(),
+    )
+    c_2b, d_2b = gen_proof_knowledge_discrete_log(3, g_1, b_2, r_2b)
+    c_3b, d_3b = gen_proof_knowledge_discrete_log(4, g_1, b_3, r_3b)
+
+    p_b = g_3.exponentiate(r_4b)
+    q_b = g_1.exponentiate(r_4b).operate(g_2.exponentiate(y))
+    c_pqb, d_pq0b, d_pq1b = gen_proof_equality_discrete_coordinates(
+        version=5,
+        g_0=g_3,
+        g_1=g_1,
+        g_2=g_2,
+        exponent_0=r_4b,
+        exponent_1=y,
+        random_value_0=r_5b,
+        random_value_1=r_6b,
+    )
 
     # Alice
+    # Checks
+    assert g_2b.value >= 2
+    assert g_3b.value >= 2
+    assert p_b.value >= 2
+    assert q_b.value >= 2
+    # Check DH first
     assert g_2b.exponentiate(a_2) == g_2
     assert g_3b.exponentiate(a_3) == g_3
-    s = get_random_secret()
-    p_a = g_3.exponentiate(s)
-    q_a = g_1.exponentiate(s).operate(g_2.exponentiate(x))
+    # Verify zk-proofs
+    assert verify_proof_knowledge_discrete_log(3, c_2b, d_2b, g_1, g_2b)
+    assert verify_proof_knowledge_discrete_log(4, c_3b, d_3b, g_1, g_3b)
+    assert verify_proof_equality_discrete_coordinates(
+        version=5,
+        g_0=g_3,
+        g_1=g_1,
+        g_2=g_2,
+        y_0=p_b,
+        y_1=q_b,
+        c=c_pqb,
+        d_0=d_pq0b,
+        d_1=d_pq1b,
+    )
+
+    r_4a, r_5a, r_6a, r_7a = (
+        get_random_secret(),
+        get_random_secret(),
+        get_random_secret(),
+        get_random_secret(),
+    )
+    p_a = g_3.exponentiate(r_4a)
+    q_a = g_1.exponentiate(r_4a).operate(g_2.exponentiate(x))
     r_a = q_a.operate(q_b.inverse()).exponentiate(a_3)
+    # NOTE: Omit the proof for `p_a` and `q_a` here, since they are generated and verified
+    #   in the same way as `p_b` and `q_b`.
+    c_ra, d_ra = gen_proof_equality_discrete_log(
+        7, g_1, q_a.operate(q_b.inverse()), a_3, r_7a,
+    )
 
     # Bob
+    # Verify zk-proofs
+    assert verify_proof_equality_discrete_log(
+        7, g_1, q_a.operate(q_b.inverse()), g_3a, r_a, c_ra, d_ra,
+    )
+
     r_b = q_a.operate(q_b.inverse()).exponentiate(b_3)
     r_ab = r_a.exponentiate(b_3)
 
